5997718783b4ab71674d686dfc3745adc0659606
AkshatAdsule <mohan.mahanth@gmail.com>
2020-08-25 15:50:54 -0700

    Add setcolor route

--- server.py
+++ server.py
@@ -1,6 +1,17 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import request
+app = Flask(__name__, static_url_path='', 
+            static_folder='static',
+            template_folder='templates')
 
 @app.route('/')
 def hello_world():
 	return 'Hello, World!'
+
+
+@app.route('/setcolor', methods=["POST"])
+def setColor():
+	r = request.args.get("R")
+	g = request.args.get("B")
+	b = request.args.get("G")
+	return(str([r,g,b]))
